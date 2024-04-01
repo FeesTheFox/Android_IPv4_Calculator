f87c699b66fb5257184a4bf9cae847d4378951e4
FeesTheFox <feesblock@gmail.com>
2024-04-01 16:36:21 +0300

    Created ActionBar with 3 menu items 1st tells you the author 2nd tells you the info about app 3nd is a home button

--- app/src/main/java/com/example/ipv4_calculator/MainActivity.java
+++ app/src/main/java/com/example/ipv4_calculator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ipv4_calculator;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
@@ -7,7 +8,10 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.ipv4_calculator.databinding.ActivityMainBinding;
 import com.example.ipv4_calculator.databinding.Fragment1Binding;
@@ -16,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int CONTENT_VIEW_ID = 10101010;
     private ActivityMainBinding binding;
+    private Fragment1 fragment1;
+    private Fragment2 fragment2;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,7 +29,37 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater()); //work with binding
         View view = binding.getRoot();
         setContentView(view);
+
+        fragment1 = new Fragment1();
+        fragment2 = new Fragment2();
+
         binding.fragment1ContainerView.setId(CONTENT_VIEW_ID);
     }
 
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        setTitle("IPv4 Calculator");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_author: // ID of the 1st item
+                Toast.makeText(this, "Fees The Fox", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.menu_info: // ID of the 2nd item
+                Toast.makeText(this, "Is an IPv4 app", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.home:
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(CONTENT_VIEW_ID,fragment1);
+                transaction.addToBackStack(null);
+                transaction.commit();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
